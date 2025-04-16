533afe0b80eb1905c5b023402a7a3b3b87a8f68e
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-04-16 14:37:43 +0400

    add main.js, make header stiky

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    /************** header on scroll  */
+    let header = document.querySelector("header");
+    let headerHeight = header.clientHeight;
+
+    window.onscroll = function() {
+        if (window.pageYOffset > headerHeight) {
+            header.classList.add("fixed");
+        } else {
+            header.classList.remove("fixed");
+        }
+    }
+
     $('.reviews__slider').slick({
         infinite: true,
         slidesToShow: 1,
